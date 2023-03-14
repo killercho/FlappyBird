62e41690e8ff4b6e0801ef9d9c6f7d90f240eb07
killercho <ggeorgi60@gmail.com>
2023-03-14 18:04:47 +0200

    Added score hitbox

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ int main()
     const int HOLE_SIZE = 100;
     const int PILON_HEIGHT = 620;
     const int PILON_WIDTH = 52;
+    const int SCORE_HEIGHT = HOLE_SIZE;
+    const int SCORE_WIDTH = 1;
     const int BIRD_HEIGHT = 24;
     const int BIRD_WIDTH = 34;
 
@@ -36,20 +38,25 @@ int main()
     int randomOffset = GetRandomValue(MINY, MAXY);
     Vector2 pilon1PosTop = { (float)400, (float)randomOffset - PILON_HEIGHT - HOLE_SIZE };
     Vector2 pilon1PosBottom = { (float)400, (float)randomOffset };
+    Vector2 score1Pos = { (float)400, (float)randomOffset - PILON_HEIGHT };
 
     randomOffset = GetRandomValue(MINY, MAXY);
     Vector2 pilon2PosTop = { (float)600, (float)randomOffset - PILON_HEIGHT - HOLE_SIZE };
     Vector2 pilon2PosBottom = { (float)600, (float)randomOffset };
+    Vector2 score2Pos = { (float)400, (float)randomOffset - PILON_HEIGHT };
 
     Rectangle birdHitbox;
 
     Rectangle pilon1TopHitbox;
     Rectangle pilon1BottomHitbox;
+    Rectangle score1Hitbox;
 
     Rectangle pilon2TopHitbox;
     Rectangle pilon2BottomHitbox;
+    Rectangle score2Hitbox;
 
     bool gameOver = false;
+    int score = 0;
 
     // Main game loop:
     while (!WindowShouldClose()) {
@@ -57,23 +64,29 @@ int main()
         birdPosition.y += GRAVITY;
         pilon1PosTop.x -= SPEED;
         pilon1PosBottom.x -= SPEED;
+        score1Pos.x -= SPEED;
         pilon2PosTop.x -= SPEED;
         pilon2PosBottom.x -= SPEED;
+        score2Pos.x -= SPEED;
 
         if (pilon1PosTop.x <= -PILON_WIDTH) {
             pilon1PosTop.x = SCREEN_WIDTH + PILON_WIDTH;
             pilon1PosBottom.x = SCREEN_WIDTH + PILON_WIDTH;
+            score1Pos.x = SCREEN_WIDTH + PILON_WIDTH;
             randomOffset = GetRandomValue(MINY, MAXY);
             pilon1PosBottom.y = randomOffset;
             pilon1PosTop.y = randomOffset - PILON_HEIGHT - HOLE_SIZE;
+            score1Pos.y = randomOffset - PILON_HEIGHT;
         }
 
         if (pilon2PosTop.x <= -PILON_WIDTH) {
             pilon2PosTop.x = SCREEN_WIDTH + PILON_WIDTH;
             pilon2PosBottom.x = SCREEN_WIDTH + PILON_WIDTH;
+            score2Pos.x = SCREEN_WIDTH + PILON_WIDTH;
             randomOffset = GetRandomValue(MINY, MAXY);
             pilon2PosBottom.y = randomOffset;
             pilon2PosTop.y = randomOffset - PILON_HEIGHT - HOLE_SIZE;
+            score2Pos.y = randomOffset - PILON_HEIGHT;
         }
 
         if (birdPosition.y < 0)
@@ -102,6 +115,13 @@ int main()
             PILON_HEIGHT,
         };
 
+        score1Hitbox = (Rectangle) {
+            score1Pos.x,
+            score1Pos.x,
+            SCORE_WIDTH,
+            SCORE_HEIGHT,
+        };
+
         pilon2TopHitbox = (Rectangle) {
             pilon2PosTop.x,
             pilon2PosTop.y,
@@ -116,6 +136,13 @@ int main()
             PILON_HEIGHT,
         };
 
+        score2Hitbox = (Rectangle) {
+            score2Pos.x,
+            score2Pos.x,
+            SCORE_WIDTH,
+            SCORE_HEIGHT,
+        };
+
         // Input
         if (IsKeyPressed(KEY_SPACE) || IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))
             birdPosition.y -= JUMPFORCE;
@@ -126,8 +153,14 @@ int main()
         hitPilon |= CheckCollisionRecs(birdHitbox, pilon2TopHitbox);
         hitPilon |= CheckCollisionRecs(birdHitbox, pilon2BottomHitbox);
 
+        bool hitScore = CheckCollisionRecs(birdHitbox, score1Hitbox);
+        hitScore |= CheckCollisionRecs(birdHitbox, score1Hitbox);
+
         if (hitPilon)
             gameOver = true;
+
+        if (hitScore)
+            score++;
 
         // Draw
         BeginDrawing();
@@ -142,8 +175,10 @@ int main()
 
             DrawTextureRec(pilon, { 0, 0, PILON_WIDTH, -PILON_HEIGHT }, pilon2PosTop, WHITE);
             DrawTextureRec(pilon, { 0, 0, PILON_WIDTH, PILON_HEIGHT }, pilon2PosBottom, WHITE);
-        } else
+        } else {
             DrawText("Game over", 0, 0, 25, BLACK);
+            DrawText(TextFormat("Your score is: %d", score), 0, 0, 25, BLACK);
+        }
         EndDrawing();
     }
 
